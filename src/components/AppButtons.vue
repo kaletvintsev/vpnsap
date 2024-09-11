96dd4386ae12e220c4340449ba9dc3eee9567d06
snork-maiden <pollykoldysheva@gmail.com>
2024-09-11 17:34:55 +0300

    Responsive promo block

---
+++ src/components/AppButtons.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="button-block">
+    <a
+      href="https://apps.apple.com/cl/app/vpn-asap-best-secure-fast/id6477047407"
+      target="_blank"
+      class="download-button"
+      ><img src="/src/assets/images/app-store.png" class="app-store" alt="apple store"
+    /></a>
+    <a href="#" class="download-button"
+      ><img src="/src/assets/images/google-store.png" class="app-store" alt="google store"
+    /></a>
+  </div>
+</template>
+<style lang="scss" scoped>
+.button-block {
+  display: flex;
+  gap: 12px;
+}
+</style>
--- src/components/LandingPromo.vue
+++ src/components/LandingPromo.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppButtons from './AppButtons.vue'
+</script>
 
 <template>
   <div class="first-block">
@@ -6,298 +8,121 @@
       <div class="mobile-logo">
         <img src="/src/assets/images/logo-mobile.png" alt="ASAP VPN" />
       </div>
-      <div class="description-block">
-        <div class="text-block">
-          <h1 class="title">
-            Surf fast.<br />
-            Talk secure.<br />
-            Stay anonymous.
-          </h1>
-          <p class="text-description">
-            Get an ultra-fast VPN and secure messenger in one app. Your online activity will be
-            completely hidden thanks to top-notch X25519 &amp; AES encryption with rock-solid
-            connections up to 10 Gbps.
-          </p>
-          <div class="button-block">
-            <a
-              href="https://apps.apple.com/cl/app/vpn-asap-best-secure-fast/id6477047407"
-              target="_blank"
-              class="download-button"
-              ><img src="/src/assets/images/app-store.png" class="app-store" alt="apple store"
-            /></a>
-            <a href="#" class="download-button"
-              ><img src="/src/assets/images/google-store.png" class="app-store" alt="google store"
-            /></a>
-          </div>
-        </div>
-        <div class="phone-block">
-          <img
-            class="phone-1"
-            src="/src/assets/images/iphone-14-pro-1.png"
-            alt="iPhone display 1"
-          />
-          <img
-            class="phone-2"
-            src="/src/assets/images/iphone-14-pro-2.png"
-            alt="iPhone display 2"
-          />
-        </div>
+      <div class="image-wrapper"></div>
 
-        <p class="text-mobile-description">
-          VPN ASAP is created to provide the best VPN solutions.<br />
-          Surf on Internet without any limitations through<br />
-          secure online connections that ensure privacy<br />
-          and security of user data.
+      <div class="description-block">
+        <h1 class="title">
+          Surf fast.<br />
+          Talk secure.<br />
+          Stay anonymous.
+        </h1>
+        <p class="text-description">
+          Get an&nbsp;ultra-fast VPN and secure messenger in&nbsp;one app. Your online activity will
+          be completely hidden thanks to&nbsp;top-notch X25519&nbsp;&amp; AES encryption with
+          rock-solid connections up&nbsp;to&nbsp;10&nbsp;Gbps.
         </p>
+        <AppButtons />
       </div>
     </div>
   </div>
 </template>
-<style lang="scss">
+
+<style lang="scss" scoped>
 .first-block {
-  display: flex;
   background-size: cover;
-  margin-top: Calc(var(--header-height)*-1);
-  padding-top: var(--header-height);
+  overflow: hidden;
   background:
     url('/src/assets/images/world-map.png') no-repeat center 60px,
     linear-gradient(315deg, #d0b336 0%, #f4d66c 100%);
-  width: 100%;
+  padding-top: 60px;
 
-  .description-block {
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    padding: 50px 0 50px 0;
-
-    .text-block {
-      flex: 1;
-      max-width: 50%;
-      margin-top: 100px;
-
-      .title {
-        font-size: 52px;
-        line-height: 1.2;
-        font-family: 'Gotham Pro', serif;
-        font-weight: 800;
-      }
-      .text-description {
-        margin-top: 20px;
-        font-size: 16px;
-        line-height: 1.4;
-        font-weight: 500;
-      }
-
-      .button-block {
-        margin-top: 20px;
-        margin-bottom: 100px;
-
-        .download-button {
-          width: 100%;
-
-          &:nth-child(2) {
-            margin-left: 15px;
-          }
-        }
-      }
-    }
-
-    .phone-block {
-      display: flex;
-      justify-content: start;
-      position: relative;
-
-      .phone-1 {
-        position: absolute;
-        width: 280px;
-        z-index: 999;
-        margin-left: -200px;
-      }
-
-      .phone-2 {
-        width: 280px;
-        z-index: 1;
-        margin-top: 50px;
-        max-height: 490px;
-      }
-    }
-
-    .text-mobile-description {
-      display: none;
-      margin-top: 40px;
-      text-align: center;
-      font-size: 16px;
-      line-height: 1.4;
-      font-weight: 500;
-      padding: 0 20px 0 20px;
-    }
-  }
-  .header {
-    width: 100%;
-    padding: 20px 0;
-    display: flex;
-    justify-content: space-between;
-
-    .logo {
-      display: flex;
-      position: relative;
-      width: 100px;
-      height: 100px;
-      background:
-        url('../assets/logo-bg.png') no-repeat center 0,
-        rgb(240, 210, 101);
-      background-blend-mode: color-dodge;
-      background-size: 100px 100px;
-      justify-content: center;
-      align-items: center;
-      
-
-      img {
-        margin-left: -5px;
-        max-width: 68px;
-      }
-    }
-
-    .menu {
-      display: flex;
-      align-items: center;
-
-      ul {
-        display: flex;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-
-        li {
-          display: inline;
-          margin-left: 30px;
-          font-size: 14px;
-          font-weight: 500;
-
-          &:first-child {
-            margin-left: 0;
-          }
-
-          a {
-            color: rgba(0, 0, 0, 0.4);
-            text-decoration: none;
-            transition: color 0.3s;
-            cursor: pointer;
-
-            &:hover {
-              color: rgba(0, 0, 0, 0.6);
-            }
-          }
-        }
-      }
-    }
-  }
-  .mobile-logo {
-    display: none;
-    position: relative;
-    width: 220px;
-    height: 226px;
-    justify-content: center;
-    align-items: center;
-    margin: auto;
-    img {
-      margin: 60px auto;
-    }
+  @media screen and (min-width: 993px) {
+    margin-top: Calc(var(--header-height) * -1);
+    padding-top: var(--header-height);
   }
 }
 
-@media only screen and (max-width: 992px) {
-  .first-block {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    // background:
-    //   url('../assets/world-map.png') no-repeat center -100px,
-    //   linear-gradient(315deg, #d0b336 0%, #f4d66c 100%);
-
-    .description-block {
-      justify-content: center;
-      align-items: center;
-      flex-flow: column;
-      padding: 0 0 50px 0;
-
-      .text-block {
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        max-width: 100%;
-        text-align: center;
-        margin-top: 0;
-
-        .title {
-          font-size: 42px;
-        }
-
-        .text-description {
-          display: none;
-        }
-
-        .button-block {
-          margin-top: 50px;
-          margin-bottom: 50px;
-        }
-      }
-
-      .phone-block {
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        max-width: 100%;
-
-        .phone-1 {
-          position: absolute;
-          width: 280px;
-          z-index: 2;
-          margin-left: -200px;
-        }
-
-        .phone-2 {
-          width: 280px;
-          z-index: 1;
-          margin-top: 50px;
-          max-height: 490px;
-          margin-left: 200px;
-        }
-      }
-
-      .text-mobile-description {
-        display: block;
-      }
-    }
-    .header {
-      display: none;
-    }
-    .mobile-logo {
-      display: grid;
-    }
+.container {
+  display: flex;
+  flex-direction: column;
+  padding: 0 24px;
+  @media screen and (min-width: 993px) {
+    flex-direction: row-reverse;
+    gap: 2em;
+    justify-content: space-between;
   }
+}
 
-  @media only screen and (max-width: 480px) {
-    .first-block {
-      .description-block {
-        .phone-block {
-          .phone-1 {
-            position: absolute;
-            width: 180px;
-            z-index: 999;
-            margin-left: -130px;
-          }
+.mobile-logo {
+  display: none;
+  width: Min(220px, 80vw);
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  margin-bottom: 20px;
+  img {
+    object-fit: cover;
+  }
+  @media only screen and (max-width: 992px) {
+    display: flex;
+  }
+}
+.image-wrapper {
+  display: flex;
+  margin: auto;
+  background-image: url('/src/assets/images/promo-phones.png');
+  width: 100%;
+  height: 700px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  z-index: 3;
+  transform: scale(1.35);
+  @media only screen and (max-width: 992px) {
+    margin-bottom: -200px;
+    height: 160vw;
+    max-width: 500px;
+    max-height: 850px;
+  }
+  @media only screen and (min-width: 993px) {
+    position: relative;
+    top: 150px;
+  }
+}
 
-          .phone-2 {
-            width: 180px;
-            z-index: 1;
-            margin-top: 50px;
-            max-height: 390px;
-            margin-left: 130px;
-          }
-        }
-      }
-    }
+.description-block {
+  display: flex;
+  flex-direction: column;
+  padding: 50px 0 50px 0;
+  justify-content: center;
+  @media only screen and (max-width: 992px) {
+    text-align: center;
+    align-items: center;
+  }
+  @media only screen and (min-width: 993px) {
+    width: 60vw;
+  }
+}
+
+.title {
+  font-size: clamp(40px, 5.5vw, 64px);
+  font-weight: 900;
+  line-height: 0.95;
+  letter-spacing: -0.04em;
+  margin-bottom: 30px;
+  @media only screen and (min-width: 993px) {
+    margin-bottom: 48px;
+  }
+}
+
+.text-description {
+  font-weight: 600;
+  line-height: 1.75;
+  letter-spacing: -0.02em;
+  margin-bottom: 50px;
+  max-width: 406px;
+  @media only screen and (min-width: 993px) {
+    margin-bottom: 58px;
   }
 }
 </style>
